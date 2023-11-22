999fde06db06b72898f786fa4e2f90db0e007cf3
Shahnawaz <shahnawaz@ranoliaventures.com>
2023-11-22 18:21:32 +0530

    updates

--- resources/views/orders/print.blade.php
+++ resources/views/orders/print.blade.php
@@ -2,6 +2,9 @@
 @section('title', 'Print')
 @section('orders', 'active')
 @section('content')
+    <style>
+
+    </style>
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <div class="container-fluid my-2">
@@ -33,23 +36,27 @@
 
                         <div class="col-md-12">
                             <div class="mb-3">
-                                <table cellpadding="3" cellspacing='3' border="0" width="700">
-                                    <thead>
-                                        <tr style="background: #ccc">
-                                            <th>#</th>
-                                            <th>Item</th>
-                                            <th>Qty</th>
-                                            <th>Total</th>
-                                            <th>Bal. Amount</th>
+                                <table style="border:1px solid #ccc" cellpadding="3" cellspacing='3' border="0"
+                                    width="700">
+                                    <thead style="background: #ccc">
+                                        <tr>
+                                            <th style="border:1px solid #ccc;text-align:center">#</th>
+                                            <th style="border:1px solid #ccc;text-align:center">Item</th>
+                                            <th style="border:1px solid #ccc;text-align:center">Qty</th>
+                                            <th style="border:1px solid #ccc;text-align:center">Total</th>
+                                            <th style="border:1px solid #ccc;text-align:center">Bal. Amount</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
+                                        <tr style="border:1px solid #ccc">
                                             <td></td>
-                                            <td>{{ $order->particular }}</td>
-                                            <td>{{ $order->qty }}</td>
-                                            <td>₹{{ $order->total_amount }}</td>
-                                            <td>₹{{ $order->total_amount - $order->discount - $advAmt }}</td>
+                                            <td style="border:1px solid #ccc;text-align:center">{{ $order->particular }}
+                                            </td>
+                                            <td style="border:1px solid #ccc;text-align:center">{{ $order->qty }}</td>
+                                            <td style="border:1px solid #ccc;text-align:center">₹{{ $order->total_amount }}
+                                            </td>
+                                            <td style="border:1px solid #ccc;text-align:center">
+                                                ₹{{ $order->total_amount - $order->discount - $advAmt }}</td>
                                         </tr>
                                     </tbody>
                                 </table>
@@ -58,12 +65,13 @@
                         <div class="col-md-12">
                             <div class="mb-3" style="justify-content: center;">
                                 <p>1. आर्डर देने पर 75% अडवांस देना होगा अन्य अपना
-                                    कार्य अधूरा समझे।</p>
-                                <p>2. 15 दिन के बाद तथा बिना पर्ची के सामान नहीं मिलेगा।</p>
-                                <p>3. अपना मैटर स्वयं चैक कर लें, गलती होने पर हमारी जिम्मेदारी नहीं होगी।</p>
-                                <p>4. आप का कार्य लेट होने पर कोई वाद मान्य नहीं होगा क्योंकि टैक्निकल प्राॅब्लम के चलते ऐसा
-                                    होना सम्भव है। </p>
-                                <p>नोट टैक्स एक्सट्रा होगा। भूलचूक लेनी देनी</p>
+                                    कार्य अधूरा समझे।<br>
+                                    2. 15 दिन के बाद तथा बिना पर्ची के सामान नहीं मिलेगा।<br>
+                                    3. अपना मैटर स्वयं चैक कर लें, गलती होने पर हमारी जिम्मेदारी नहीं होगी।<br>
+                                    4. आप का कार्य लेट होने पर कोई वाद मान्य नहीं होगा क्योंकि टैक्निकल प्राॅब्लम के चलते
+                                    ऐसा
+                                    होना सम्भव है।<br>
+                                    नोट टैक्स एक्सट्रा होगा। भूलचूक लेनी देनी</p>
                             </div>
                         </div>
                     </div>
